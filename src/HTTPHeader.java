2222764809506a4b53e99cfde68769cb29018fbb
Dmytro Shpak <dshpak.work@gmail.com>
2022-10-28 00:23:24 +0300

    add class HTTPHeaders

---
+++ src/HTTPHeader.java
@@ -0,0 +1,9 @@
+public class HTTPHeader {
+    String name;
+    String value;
+
+    public HTTPHeader (String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+}
--- src/Request.java
+++ src/Request.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Request {
     String method;
     String command;
     String body;
     String endpoint;
+    ArrayList <HTTPHeader> headers;
+
     public Request(String method, String command, String body){
         this.method = method;
         this.command = command;
         this.body = body;
         this.endpoint = this.method + " " + this.command;
+        this.headers = new ArrayList<>();
     }
 
     public String getEndpoint(){
