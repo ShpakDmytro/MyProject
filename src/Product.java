d81a7ffa02bcb4e4aa434ec0fc568910c61b3c13
Dmytro Shpak <dshpak.work@gmail.com>
2022-10-30 09:35:55 +0200

    add MySQL db for saving

--- src/Product.java
+++ src/Product.java
@@ -2,34 +2,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Product {
-    int id;
+    String id;
     String name;
     double price;
-    ArrayList<User> userBuy;
+    //ArrayList<User> userBuy; need update
 
-    public Product(int id, String name, double price) {
+    public Product(String id, String name, double price) {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.userBuy = new ArrayList<>();
+       // this.userBuy = new ArrayList<>();
     }
 
-    public void addUser(User user) {
-        userBuy.add(user);
-    }
+    //public void addUser(User user) {
+    //    userBuy.add(user);
+    //}
 
-    public int howManyUsers() {
-        return userBuy.size();
-    }
+    //public int howManyUsers() {
+    //    return userBuy.size();
+    //}
 
-    public User getUserAtIndex(int index) throws Exception {
-        if (index > userBuy.size() || index < 0) {
-            throw new Exception("Index wrong");
-        }
-        return userBuy.get(index);
-    }
+    //public User getUserAtIndex(int index) throws Exception {
+    //    if (index > userBuy.size() || index < 0) {
+    //        throw new Exception("Index wrong");
+    //    }
+    //    return userBuy.get(index);
+    //}
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
@@ -46,15 +46,15 @@ public class Product {
         product.put("id",this.id);
         product.put("name",this.name);
         product.put("price",this.price);
-        ArrayList <HashMap> userBuyList = new ArrayList<>();
-        for (User info : this.userBuy) {
-            HashMap<String, Object> user = new HashMap<>();
-            user.put("id", info.getId());
-            user.put("firstName", info.getFirstName());
-            user.put("lastName", info.getLastName());
-            userBuyList.add(user);
-        }
-        product.put("userBuy",userBuyList);
+        //ArrayList <HashMap> userBuyList = new ArrayList<>();
+        //for (User info : this.userBuy) {
+        //    HashMap<String, Object> user = new HashMap<>();
+        //    user.put("id", info.getId());
+        //    user.put("firstName", info.getFirstName());
+        //    user.put("lastName", info.getLastName());
+        //    userBuyList.add(user);
+        //}
+        //product.put("userBuy",userBuyList);
         return product;
     }
 }
